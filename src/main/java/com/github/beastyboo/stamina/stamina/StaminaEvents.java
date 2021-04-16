e9a7eb6dedbe2083966d20ebf52e6be2b9c7f37f
BeastyBoo <torbjorn1.skavhaug@gmail.com>
2021-04-16 17:35:39 +0200

    Added safety for StaminaPlayer by overriding .equals and hashcode

--- src/main/java/com/github/beastyboo/stamina/stamina/StaminaEvents.java
+++ src/main/java/com/github/beastyboo/stamina/stamina/StaminaEvents.java
@@ -4,7 +4,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.Map;
